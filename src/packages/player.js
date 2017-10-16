26871d8019adf4fd96bca20abd663117f2494163
SrPatinhas <migas_cerejo-1993@hotmail.com>
2017-10-16 16:38:03 +0100

    add more tweaks on VideoJs and the player

--- src/packages/player.js
+++ src/packages/player.js
@@ -1,26 +1,65 @@
-module.exports = {
-	/*videojs(document.querySelector('.video-js'), {
-				src: "",
-				controls: true,
-				autoplay: false,
-				preload: 'auto'
-			});*/
+/*videojs(document.querySelector('.video-js'), {
+			src: "",
+			controls: true,
+			autoplay: false,
+			preload: 'auto'
+		});*/
 
-	play: function (path){
-		var options = {
-			src: path;
+//https://developers.google.com/cast/docs/styled_receiver
+
+var create = function(element){
+	console.log("Create Player");
+	document.getElementById('video-player').innerHTML += '<video id="my-player" class="video-js"><p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that<a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p></video>';
+};
+
+var play = function (path){
+	create();
+
+	var options = {
+			sources: [{
+				type: "video/mp4",
+				src: path
+			}],
+			controls: true,
+			"playbackRates": [1, 2]/*,
+			chromecast:{
+				appId: 'APP-ID',
+				metadata: {
+					title: 'Title display on tech wrapper',
+					subtitle: 'Synopsis display on tech wrapper',
+				}
+			}*/
 		};
-		 
-		var player = videojs(document.getElementById('my-player'), options, function onPlayerReady() {
-			videojs.log('Your player is ready!');
 
-			// In this context, `this` is the player that was created by Video.js.
-			this.play();
+	var player = videojs( document.getElementById('my-player'), options, function onPlayerReady() {
+		document.getElementById('my-player').parentElement.classList = "video-player show-player";
+		videojs.log('Your player is ready!');
 
-			// How about an event listener?
-			this.on('ended', function() {
-				videojs.log('Awww...over so soon?!');
-			});
+		// In this context, `this` is the player that was created by Video.js.
+		this.play();
+
+		// to close Player
+		document.getElementsByClassName("close-player")[0].addEventListener("click", (e) => {
+			destroy(document.getElementById('my-player'));
 		});
-	}
+
+		// How about an event listener?
+		this.on('ended', function() {
+			videojs.log('Awww...over so soon?!');
+			destroy(document.getElementById('my-player'));
+		});
+	});
+	player.selectSubtitle();
+};
+
+var destroy = function(element){
+	console.log("Player Destroyed");
+	element.parentElement.classList = "video-player";
+	videojs( element ).dispose();
+};
+
+module.exports = {
+	create: create,
+	play: play,
+	destroy: destroy
 }
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -7,12 +7,16 @@ const menu 				= new Menu();
 
 const fs 			= require('fs');
 var request 		= require('request');                       //to make http requests
-//var zlib 			= require('zlib');                             //to read zipped files
-var iconv 			= require('iconv-lite');                      //to recode files
-//var videojs 		= require('videojs');						//video js 
+//var zlib 			= require('zlib');                          //to read zipped files
+var iconv 			= require('iconv-lite');                    //to recode files
+
+var videojs 		= require('video.js');						//video js 
+//var videojsChromecast = 
+require("videojs-chromecast");
+require('videojs-select-subtitle');								// The actual plugin function is exported by this module, but it is also  attached to the `Player.prototype`; so, there is no need to assign it  to a variable. 
 var _				= require('underscore');
 //https://github.com/vankasteelj/opensubtitles-api
-var OpenSubtitles 	= require('opensubtitles-api');       //for subtitles
+var OpenSubtitles 	= require('opensubtitles-api');       		//for subtitles
 //create new instance
 var OS 				= new OpenSubtitles('Butter');
 //packages
@@ -20,7 +24,7 @@ var topbar 		= require('./../packages/topbar.js');
 var util_tools	= require('./../packages/util.js');
 var folder_list = require('./../packages/folder_list.js');
 var subs_search = require('./../packages/subs_search.js');
-//var player 		= require('./../packages/player.js');
+var player 		= require('./../packages/player.js');
 
 
 
